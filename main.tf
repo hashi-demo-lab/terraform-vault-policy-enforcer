9435f536a0719a57338ba1a6434ef3b2a9e65e25
Aaron Evans <aarone@outlook.com.au>
2024-10-15 21:33:07 +1100

    Add userpass authentication and security audit user configuration in Terraform

--- main.tf
+++ main.tf
@@ -46,3 +46,36 @@ resource "vault_jwt_auth_backend_role" "demo" {
     "name"  = "name"
   }
 }
+
+resource "vault_auth_backend" "userpass" {
+  type = "userpass"
+}
+
+resource "vault_generic_endpoint" "security_audit_user" {
+  depends_on           = [vault_auth_backend.userpass]
+  path                 = "auth/userpass/users/security_audit_user"
+  ignore_absent_fields = true
+  data_json            = <<EOT
+  {
+    "password": "password"
+  }
+  EOT
+}
+
+resource "vault_identity_entity" "security_audit_user_entity" {
+  name     = "security_audit_user"
+  policies = [vault_policy.export_keys.name]
+  metadata = {
+    organization = "Security Team"
+    role         = "Audit"
+  }
+}
+
+# Create an alias to associate the entity with the userpass auth method
+resource "vault_identity_entity_alias" "security_audit_user_alias" {
+  name           = "security_audit_user"                               # The username in userpass auth method
+  mount_accessor = vault_auth_backend.userpass.accessor                # Refers to the userpass auth method
+  canonical_id   = vault_identity_entity.security_audit_user_entity.id # The ID of the entity
+}
+
+
