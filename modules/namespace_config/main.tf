57124eb9290b8433bb6be8bfbb2943ca2214d8ac
Aaron Evans <aarone@outlook.com.au>
2024-10-18 12:35:26 +1100

    Update OIDC role configuration to use dynamic role name and allowed redirect URIs

--- modules/namespace_config/main.tf
+++ modules/namespace_config/main.tf
@@ -11,10 +11,10 @@ terraform {
 resource "vault_jwt_auth_backend_role" "oidc_role_dev" {
   namespace      = var.namespace  # Use the namespace variable
   backend        = var.oidc_backend_path  # Reference the OIDC backend path from the oidc module
-  role_name      = "dev-role"
+  role_name      = "demo"
   token_policies = ["dev"]
 
-  allowed_redirect_uris = ["https://example.com/callback"]
+  allowed_redirect_uris = var.allowed_redirect_uris
   user_claim            = "email"
   claim_mappings = {
     "email" = "email"
