74905d4db11a429a1bb99670ac36350866376e3d
Aaron Evans <aarone@outlook.com.au>
2024-10-18 12:35:39 +1100

    Add allowed_redirect_uris variable to namespace configuration

--- modules/namespace_config/variables.tf
+++ modules/namespace_config/variables.tf
@@ -7,3 +7,9 @@ variable "oidc_backend_path" {
   description = "The path of the OIDC backend"
   type        = string
 }
+
+variable "allowed_redirect_uris" {
+  description = "Allowed redirect URIs for the OIDC role"
+  type        = list(string)
+}
+
